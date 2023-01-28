873b1a21f085aa8d3b61a981ad218cedc3bccccc
pm-User 04 <pm-User04@edu.bfi.wien>
2023-01-28 14:35:27 +0100

    loop is working

--- infinite_slider/script.js
+++ infinite_slider/script.js
@@ -14,22 +14,21 @@ let left = 0;
 function clone() {
     let slides = document.querySelectorAll(".slide");
     let clone = slides[0].cloneNode(true);
+    sliderContainer.appendChild(clone);
     sliderContainer.removeChild(slides[0]);
-    sliderContainer.appendChild(clone)
+    sliderContainer.style.left = 0;
+
 }
 
 
 
 function moveBox() {
-    left--;
     sliderContainer.style.left = `${left}px`;
-    console.log(left)
-    if (sliderContainer.style.left === "-200px") {
-        console.log("test");
-        clone();
+    left--;
+    if (sliderContainer.style.left === "-500px") {
         left = 0;
-
+        clone();
     }
 }
 
-setInterval(moveBox, 10);
+setInterval(moveBox, 20);
